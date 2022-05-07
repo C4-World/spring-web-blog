42bc19883f81989a4883b644bd3825951a762242
Allen <kycoolcool@gmail.com>
2022-05-07 10:25:32 +0800

    feat: 添加日志功能，使用 Slf4j 注解

--- src/main/java/com/kycool/blog/controller/World.java
+++ src/main/java/com/kycool/blog/controller/World.java
@@ -1,15 +1,18 @@
 package com.kycool.blog.controller;
 
 import com.kycool.blog.response.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @ResponseResult
 public class World {
 
     @GetMapping("/world")
     public String getWorld() {
+        log.info("----this recording world log");
         return "world";
     }
 
